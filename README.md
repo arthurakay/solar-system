8999ffa65d69f0c6189e3a97bb55c1a11a655f49
Arthur Kay <arthurakay@gmail.com>
2018-07-23 08:58:42 -0500

    react app setup

---
+++ README.md
@@ -0,0 +1,25 @@
+# WebGL Solar System
+
+This project uses Three.js and React to produce a WebGL visualization of the solar system.
+
+_It is very much a work-in-progress._
+
+## Setup and architecture
+
+```
+    yarn
+```
+
+The application is built using [create-react-app](https://github.com/facebook/create-react-app).
+
+## Run
+
+```
+    yarn start
+```
+
+## Build
+
+```
+    yarn build
+```
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">WebGL Solar System</h1>
+
+                    <p className="App-intro">
+                        Eventually add some intro text here...
+                    </p>
+                </header>
+
+                <section className="App-body">
+                    the app
+                </section>
+
+            </div>
+        );
+    }
+}
+
+export default App;
